8141959398f3e8e8a79bc773a979c14e91301467
RTYI9 <69012366+RTYI9@users.noreply.github.com>
2020-07-30 21:29:50 +0300

    Add files via upload

---
+++ 8r9-termux.py
@@ -0,0 +1,421 @@
+import os
+os.system ("clear")
+
+col = "\033[0;31m"
+
+co = "\033[0;32m"
+
+c = "\033[0;33m"
+
+l = "\033[1;35m"
+
+h = "\033[0;34m"
+
+print (""+col)
+print ("            _____   _____    _____"+co)
+print ("           /  _  \ |  _  \  /  _  \ "+c)
+print ("           | |_| | | |_| |  | |_| | "+l)
+print ("           }  _  { |  _  /  \___  | "+h)
+print ("           | |_| | | | \ \   ___| | "+col)
+print ("           \_____/ |_|  \_\ |_____/\n\n\n "+c)
+print ("        ______________________________")
+print ("              My Chanel : 8R9AN")
+print ("        ______________________________")
+print ("             My Insta : asta_199")
+print ("        ______________________________")
+print ("                Groub : 8RA9NA \n\n\n\n\n")
+
+
+print ("\033[0;34m            P A S S W O R D :    8R9\n\n\n\n")
+
+pas = input ("\033[0;35m          \033[0;32m    Enter Password : ")
+
+if pas == "8R9" :
+    print ("OK...\n\n\n")
+    print ("\n\n  \033[0;35m                [1] \033[0;34m WishFish")
+    print ("\n\n  \033[0;35m                [2] \033[0;34m Saycheese")
+    print ("\n\n  \033[0;35m                [3] \033[0;34m MobDroid")
+    print ("\n\n  \033[0;35m                [4] \033[0;34m Metasploit")
+    print ("\n\n  \033[0;35m                [5] \033[0;34m SNINSTA")
+    print ("\n\n  \033[0;35m                [6] \033[0;34m TBomb")
+    print ("\n\n  \033[0;35m                [7] \033[0;34m ip-Tracker")
+    print ("\n\n  \033[0;35m                [8] \033[0;34m Virus4")
+    print ("\n\n  \033[0;35m                [9] \033[0;34m MRking")
+    print ("\n\n  \033[0;35m                [10] \033[0;34m ShelPhish")
+    print ("\n\n  \033[0;35m                [11] \033[0;34m sqlmap")
+    print ("\n\n  \033[0;35m                [12] \033[0;34m Virus-droid-creator")
+    print ("\n\n  \033[0;35m                [13] \033[0;34m Report")
+    print ("\n\n  \033[0;35m                [14] \033[0;34m virus_bot")
+    print ("\n\n  \033[0;35m                [15] \033[0;34m Card-Number")
+    print ("\n\n\n\n\n\n  \033[0;35m                [16] \033[0;34m Termux")
+    print ("\n\n  \033[0;35m                [17] \033[0;34m install *")
+
+
+
+else :
+    print ("ERROR... ENTER PASSWORD..."*100)
+
+print (""+col)
+inp = input ("\n\n\n             Type Number »  ")
+
+if inp == "1" :
+
+    os.system("pkg install git -y  ")
+    os.system("rm -rif ngrok")
+    os.system("rm -rif WishFish")
+    os.system("pkg install php -y")
+    os.system("pkg install openssh -y")
+    os.system("git clone https://github.com/kinghacker0/WishFish")
+    os.system("pkg install wget -y")
+    os.system("wget https://raw.githubusercontent.com/Cesar-Hack-Gray/release-download/master/fix-ar-ngrok && bash fix-ar-ngrok")
+
+    os.system("cd WishFish")
+    os.system("cp $PREFIX/bin/ngrok ~/WishFish")
+    os.system("cd")
+    print (""+col)
+    print ("\n\n\n\n\n new Type 'cd WishFish'  'wishfish.sh'")
+
+if inp == "3" :
+    os.system("pkg install git -y")
+    os.system("pkg install python2 -y")
+    os.system("git clone https://github.com/kinghacker0/Mob-Droid")
+    print (""+col)
+    print ("\n\n\n\n\n Type 'cd Mob-Droid'     'python2 mob-droid-termux.py'")
+
+if inp == "5" :
+
+    os.system("pkg install php")
+    os.system("pkg install nano")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pkg install python2")
+    os.system("git clone https://github.com/SHANIBSN/SNINSTA")
+    print ("\n\n\n\n\n Type :  cd SNINSTA    python2 sn.hacker.py")
+
+if inp == "6" :
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/TheSpeedX/TBomb.git")
+    print ("\n\n\n\n Yype  :  cd TBomb   bash TBomb.sh")
+
+if inp == "7" :
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install python2 -y")
+    os.system("pkg install php -y")
+    os.system("pkg install openssh -y")
+    os.system("git clone https://github.com/rajkumardusad/IP-Tracer.git")
+    print (" Type :  cd IP-Tracer   sh install or ./install")
+
+if inp == "8" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/Amerlaceset/Amer")
+    print ("Type :  cd Amer    bash Amer.sh")
+
+if inp == "9" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/king-hacking/MRKING.git")
+    print (" Type  :  cd MRKING   ./MRKING.sh")
+
+if inp == "10" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("https://github.com/thelinuxchoice/shellphish")
+    print (" Type : cd shellphish   bash shellphish.sh")
+
+if inp == "11" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    print (" Type :  cd sqlmap   python2  sqlmap.py")
+
+if inp == "12" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/saydog/Virus-droid-creato")
+    print ("Type cd cd Virus-droid-creator     python2 dog.py")
+
+if inp == "13" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/Ranginang67/DarkFly-Tool")
+    print ("Type :  cd DarkFly-Tool   python2 DarkFly-Tool")
+
+if inp == "14" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/princeyasinzone/virus_bot")
+    print (" Type : cd virus_bot   php virus_bot")
+
+if inp == "15" :
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/INDOnimous/Card-Number")
+    print ("Type : cd Card-Number   sh Card.sh")
+
+
+if inp == "4" :
+
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("pkg install unstable-repo -y")
+    os.system("pkg install metasploit -y")
+    os.system("apt -f install -y")
+    print ("Type : msfconsole")
+
+if inp == "16" :
+
+    os.system("apt update -y")
+
+
+    os.system("apt upgrade -y ")
+
+
+    os.system("pkg install update -y")
+
+
+    os.system("pkg install upgrade -y")
+
+
+    os.system("termux-setup-storag")
+
+    os.system("apt-get update -y")
+
+
+    os.system("apt-get upgrade -y")
+
+
+    os.system("apt install php -y")
+
+
+    os.system("apt install python -y")
+
+
+    os.system("apt install python2 -y")
+
+
+    os.system("apt install git -y")
+
+
+    os.system("apt install golang -y")
+
+
+    os.system("apt install host -y")
+
+
+    os.system("apt install nano -y")
+
+
+    os.system("apt install havij -y") 
+
+
+    os.system("apt install hydra -y")
+
+
+    os.system("apt install wireshark -y")
+
+
+    os.system("apt install cmatrix -y")
+
+
+    os.system("pkg install figlet -y")
+
+
+    os.system("pkg install wget-y")
+
+
+    os.system("pkg install wget -y")
+
+
+    os.system("pkg install python2 -y")
+
+
+    os.system("pkg install python2-dev -y")
+
+
+    os.system("apt install wireshark -y")
+
+
+    os.system("pkg install cowsay -y")
+
+
+    os.system("pkg install toilet -y")
+
+
+    os.system("pkg install ruby -y")
+
+    os.system("pkg install help -y")
+
+
+    os.system("gem install lolcat")
+
+
+    os.system("pkg install curl -y")
+
+
+    os.system("pkg install wgetrc -y")
+
+
+    os.system("pkg install unzip -y") 
+
+
+    os.system("pkg install openssh -y")
+
+
+    os.system("pkg install tor -y")
+
+
+    os.system("pkg install uzip -y")
+
+
+    os.system("pkg install net-tools -y") 
+
+
+    os.system(" pkg install unrar -y")
+
+
+    os.system(" pkg install proot -y")
+
+
+    os.system("pip2 install wget -y")
+
+
+    os.system("pip2 install requests")
+
+
+
+
+if inp == "17" :
+
+    os.system("pkg install git -y  ")
+    os.system("rm -rif ngrok")
+    os.system("rm -rif WishFish")
+    os.system("pkg install php -y")
+    os.system("pkg install openssh -y")
+    os.system("git clone https://github.com/kinghacker0/WishFish")
+    os.system("pkg install wget -y")
+    os.system("wget https://raw.githubusercontent.com/Cesar-Hack-Gray/release-download/master/fix-ar-ngrok && bash fix-ar-ngrok")
+
+    os.system("cd WishFish")
+    os.system("cp $PREFIX/bin/ngrok ~/WishFish")
+
+
+
+    os.system("pkg install git -y")
+    os.system("pkg install python2 -y")
+    os.system("git clone https://github.com/kinghacker0/Mob-Droid")
+
+
+    os.system("pkg install php")
+    os.system("pkg install nano")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pkg install python2")
+    os.system("git clone https://github.com/SHANIBSN/SNINSTA")
+
+
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/TheSpeedX/TBomb.git")
+
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install python2 -y")
+    os.system("pkg install php -y")
+    os.system("pkg install openssh -y")
+    os.system("git clone https://github.com/rajkumardusad/IP-Tracer.git")
+
+
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/Amerlaceset/Amer")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/king-hacking/MRKING.git")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("https://github.com/thelinuxchoice/shellphish")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/sqlmapproject/sqlmap")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/saydog/Virus-droid-creato")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/Ranginang67/DarkFly-Tool")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/princeyasinzone/virus_bot")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("git clone https://github.com/INDOnimous/Card-Number")
+
+    os.system("pkg install git -y")
+    os.system("pkg install python -y")
+    os.system("pkg install openssh -y")
+    os.system("pkg install php -y")
+    os.system("pkg install unstable-repo -y")
+    os.system("pkg install metasploit -y")
+    os.system("apt -f install -y")
+
+
